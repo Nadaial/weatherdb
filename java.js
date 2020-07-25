5d86f16f05d1d05f42cd21150dbde8fa43298282
Nada Al <nadaabulibdeh@yahoo.ca>
2020-07-24 23:55:58 -0400

    added js

--- java.js
+++ java.js
@@ -7,4 +7,22 @@ var searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
 
-function setQuery()
+function setQuery(event) {
+    if (event.keycode== 13){
+    getResults(searchbox.value);
+    console.log(searchbox.value);
+}
+}
+
+function getResults(query){
+
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+
+    }).then(displayResults);
+}
+
+function displayResults(weather){
+    console.log(weather);
+}
